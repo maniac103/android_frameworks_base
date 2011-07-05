c5b76bb6d1d9626e9e2f5518442c5fcf4093ab20
Danny Baumann <dannybaumann@web.de>
2011-07-05 09:55:41 +0200

    Generalize location proxy loading into vendor specific service loading.

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -366,13 +366,6 @@ class ServerThread extends Thread {
             } catch (Throwable e) {
                 Slog.e(TAG, "Failure starting Location Manager", e);
             }
-// Motorola Location Proxy
-            try {
-                Slog.i(TAG, "Location Proxy Service");
-            } catch (Throwable e) {
-                Slog.e(TAG, "Failure starting Location Proxy Service", e);
-            }
-//end
             try {
                 Slog.i(TAG, "Search Service");
                 ServiceManager.addService(Context.SEARCH_SERVICE,
@@ -488,24 +481,41 @@ class ServerThread extends Thread {
             } catch (Throwable e) {
                 Slog.e(TAG, "Failure starting AssetRedirectionManager Service", e);
             }
-        
-//Need exclude this code at other devices.
-           Object instance = null;
-	     try {
-	    	Slog.i(TAG, "Location Proxy Service");
-            	DexClassLoader cl =  new DexClassLoader("/system/framework/com.motorola.android.location.jar", new ContextWrapper(context).getCacheDir().getAbsolutePath(), null, ClassLoader.getSystemClassLoader());
-            	Class<?> klass = cl.loadClass("com.android.server.LocationProxyService");
-            if (klass != null) {
-            	Constructor<?> ctor = klass.getDeclaredConstructors()[0];
-                instance = ctor.newInstance(context);
-		if (instance != null && instance instanceof IBinder) {
-			ServiceManager.addService(klass.getSimpleName(), (IBinder) instance); 
-	       }
+
+            String[] vendorServices = context.getResources().getStringArray(
+                    com.android.internal.R.array.config_vendorServices);
+
+            if (vendorServices != null && vendorServices.length > 0) {
+                String cachePath = new ContextWrapper(context).getCacheDir().getAbsolutePath();
+                ClassLoader parentLoader = ClassLoader.getSystemClassLoader();
+
+                for (String service : vendorServices) {
+                    String[] parts = service.split(":");
+                    if (parts.length != 2) {
+                        Slog.e(TAG, "Found invalid vendor service " + service);
+                        continue;
+                    }
+
+                    String jarPath = parts[0];
+                    String className = parts[1];
+
+                    try {
+                        /* Intentionally skipping all null checks in this block, as we also want an
+                           error message if class loading or ctor resolution failed. The catch block
+                           conveniently provides that for us also for NullPointerException */
+                        DexClassLoader loader = new DexClassLoader(jarPath, cachePath, null, parentLoader);
+                        Class<?> klass = loader.loadClass(className);
+                        Constructor<?> ctor = klass.getDeclaredConstructors()[0];
+                        Object instance = ctor.newInstance(context);
+
+                        ServiceManager.addService(klass.getSimpleName(), (IBinder) instance);
+                        Slog.i(TAG, "Vendor service " + className + " started.");
+                    } catch (Exception e) {
+                        Slog.e(TAG, "Starting vendor service " + className + " failed.", e);
+                    }
+                }
             }
-        } catch (Exception e) {
-               Slog.e(TAG, "Failure starting Location Proxy Service", e);
         }
-}
 
         // make sure the ADB_ENABLED setting value matches the secure property value
         Settings.Secure.putInt(mContentResolver, Settings.Secure.ADB_ENABLED,
