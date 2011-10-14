66713db36aa500f054fbd8880ea3300f16bb6cfe
Cuong Bui <cuongbui@gmail.com>
2011-10-14 11:48:27 +0200

    Support for BT GPS dongles
    see http://iloki.nl and http://code.google.com/p/android/issues/detail?id=9057
    
    Change-Id: I2e6e6071f5d4ae553f12f3aa4989d085820d787d

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3730,6 +3730,12 @@ public final class Settings {
          * @hide
          */
         public static final String ASSISTED_GPS_ENABLED = "assisted_gps_enabled";
+        
+         /**
+         * External BT GPS device
+         * @hide
+         */
+        public static final String EXTERNAL_GPS_BT_DEVICE = "0";
 
         /**
          * The Logging ID (a unique 64-bit value) as a hex string.
@@ -5097,4 +5103,5 @@ public final class Settings {
     public static String getGTalkDeviceId(long androidId) {
         return "android-" + Long.toHexString(androidId);
     }
+    
 }
