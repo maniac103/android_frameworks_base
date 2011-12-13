552e7f1b41fa3f352b8c5d4fc4859cbd8f129153
Danny Baumann <dannybaumann@web.de>
2011-12-13 13:54:05 +0100

    Avoid magic numbers.

--- policy/src/com/android/internal/policy/impl/LockScreen.java
+++ policy/src/com/android/internal/policy/impl/LockScreen.java
@@ -62,6 +62,7 @@ import android.os.SystemProperties;
 import android.os.Vibrator;
 import android.preference.MultiSelectListPreference;
 import android.provider.Settings;
+import android.provider.CmSystem.LockscreenStyle;
 import android.provider.CmSystem.RotaryStyle;
 import android.provider.CmSystem.RinglockStyle;
 import android.content.pm.ActivityInfo;
@@ -223,12 +224,15 @@ class LockScreen extends LinearLayout implements KeyguardScreen, KeyguardUpdateM
     private boolean mRotaryHideArrows = (Settings.System.getInt(mContext.getContentResolver(),
             Settings.System.LOCKSCREEN_ROTARY_HIDE_ARROWS, 0) == 1);
 
-    private boolean mUseRotaryLockscreen = (mLockscreenStyle == 2);
+    private boolean mUseRotaryLockscreen =
+        LockscreenStyle.getStyleById(mLockscreenStyle) == LockscreenStyle.Rotary;
 
-    private boolean mUseLenseSquareLockscreen = (mLockscreenStyle == 4);
+    private boolean mUseLenseSquareLockscreen =
+        LockscreenStyle.getStyleById(mLockscreenStyle) == LockscreenStyle.Lense;
     private boolean mLensePortrait = false;
 
-    private boolean mUseRingLockscreen = (mLockscreenStyle == 5);
+    private boolean mUseRingLockscreen =
+        LockscreenStyle.getStyleById(mLockscreenStyle) == LockscreenStyle.Ring;
 
     private boolean mRingUnlockMiddle = (Settings.System.getInt(mContext.getContentResolver(),
             Settings.System.LOCKSCREEN_RING_UNLOCK_MIDDLE, 0) == 1);
