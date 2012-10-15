59ea5e7574474d65af45d5d40a5026d397bf7f33
Danny Baumann <dannybaumann@web.de>
2012-10-15 11:23:27 +0200

    Attempt to workaround missing responses to SETUP_DATA_CALL RIL requests.

--- telephony/java/com/android/internal/telephony/MotoWrigley3GRIL.java
+++ telephony/java/com/android/internal/telephony/MotoWrigley3GRIL.java
@@ -18,11 +18,13 @@ package com.android.internal.telephony;
 
 import android.content.Context;
 import android.os.AsyncResult;
+import android.os.Looper;
 import android.os.Message;
 import android.os.Parcel;
 import android.util.Log;
 
 import com.android.internal.telephony.DataCallState;
+import static com.android.internal.telephony.RILConstants.RIL_REQUEST_SETUP_DATA_CALL;
 import com.android.internal.telephony.gsm.NetworkInfo;
 import com.android.internal.telephony.gsm.SuppServiceNotification;
 
@@ -38,6 +40,7 @@ public class MotoWrigley3GRIL extends RIL {
     private static final String TAG = "MotoWrigley3GRIL";
 
     private int mDataConnectionCount = -1;
+    private RILRequest mSetupDataCallRequest;
 
     public MotoWrigley3GRIL(Context context) {
         super(context);
@@ -45,6 +48,22 @@ public class MotoWrigley3GRIL extends RIL {
 
     public MotoWrigley3GRIL(Context context, int networkMode, int cdmaSubscription) {
         super(context, networkMode, cdmaSubscription);
+        mSender = new RILSender(mSenderThread.getLooper());
+    }
+
+    @Override
+    protected RILRequest
+    findAndRemoveRequestFromList(int serial) {
+        RILRequest rr = super.findAndRemoveRequestFromList(serial);
+        if (rr == mSetupDataCallRequest) {
+            /*
+             * either response arrived, or there was an error that was already handled -
+             * either way, the upper layers were notified already
+             */
+            Log.d(TAG, "Got SETUP_DATA_CALL response");
+            mSetupDataCallRequest = null;
+        }
+        return rr;
     }
 
     @Override
@@ -82,6 +101,34 @@ public class MotoWrigley3GRIL extends RIL {
 
     @Override
     public void
+    setupDataCall(String radioTechnology, String profile, String apn,
+            String user, String password, String authType, String protocol,
+            Message result) {
+        RILRequest rr
+                = RILRequest.obtain(RIL_REQUEST_SETUP_DATA_CALL, result);
+
+        rr.mp.writeInt(7);
+
+        rr.mp.writeString(radioTechnology);
+        rr.mp.writeString(profile);
+        rr.mp.writeString(apn);
+        rr.mp.writeString(user);
+        rr.mp.writeString(password);
+        rr.mp.writeString(authType);
+        rr.mp.writeString(protocol);
+
+        if (RILJ_LOGD) riljLog(rr.serialString() + "> "
+                + requestToString(rr.mRequest) + " " + radioTechnology + " "
+                + profile + " " + apn + " " + user + " "
+                + password + " " + authType + " " + protocol);
+
+        mSetupDataCallRequest = rr;
+
+        send(rr);
+    }
+
+    @Override
+    public void
     deactivateDataCall(int cid, Message result) {
         if (mDataConnectionCount == 0) {
             Log.w(TAG, "Received deactivateDataCall RIL call without an active data call, dropping");
@@ -89,6 +136,30 @@ public class MotoWrigley3GRIL extends RIL {
             result.sendToTarget();
         } else {
             super.deactivateDataCall(cid, result);
+        }
+    }
+
+    class RILSender extends RIL.RILSender {
+        public RILSender(Looper looper) {
+            super(looper);
+        }
+
+        @Override
+        public void
+        handleMessage(Message msg) {
+            super.handleMessage(msg);
+
+            if (msg.what == EVENT_WAKE_LOCK_TIMEOUT && mSetupDataCallRequest != null) {
+                MotoWrigley3GRIL.super.findAndRemoveRequestFromList(mSetupDataCallRequest.mSerial);
+                if (mSetupDataCallRequest.mResult != null) {
+                    Log.e(TAG, "Got stale SETUP_DATA_CALL request, pretending radio not available");
+                    CommandException ex = new CommandException(CommandException.Error.RADIO_NOT_AVAILABLE);
+                    AsyncResult.forMessage(mSetupDataCallRequest.mResult, null, ex);
+                    mSetupDataCallRequest.mResult.sendToTarget();
+                }
+                mSetupDataCallRequest.release();
+                mSetupDataCallRequest = null;
+            }
         }
     }
 }
